7c0f9edd4e3560f9afa0b7961136679db47154e8
Adithya Nair <adithyanair121@gmail.com>
2024-11-18 22:00:20 +0530

    Code restructure and solved exercise 1.1 and 1.2

---
+++ code/chapter-1/exercise.lisp
@@ -0,0 +1,26 @@
+;; Exercise problems from Chapter 1
+;; Exercise 1.1
+;; Define a function last-name that returns the last name of a name.
+(defparameter *Suffixes* '(Jr. MD Sir Sr.))
+
+(defun last-name (name)
+  "Defining a last name finder for a full name"
+  (if (member (first (last name)) *Suffixes*)
+      (last-name (butlast name))
+      (car (last name))))
+
+(setf names '((Rex Morgan MD) (Carnival Sir) (Megan Markle Sr.)))
+
+(mapcar #'last-name names)
+
+;; Exercise 1.2
+;; Define a function for exponentiation.
+(defun power (number exponent)
+  "Returns a number raised to the power."
+  (if (not (equalp exponent 2))
+      (* number (power number (+ exponent -1)))
+      (* number number)
+      )
+  )
+;; Works only for exponents *above* 2
+
